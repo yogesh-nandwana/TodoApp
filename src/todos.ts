be5329a8fe18acb2ddf4573bf3dbe29ecb5a7147
Yogesh Nandwana <yogesh.nandwana@gmail.com>
2020-05-18 11:34:58 +0530

    created db.json

---
+++ src/todos.ts
@@ -0,0 +1,11 @@
+export let todos =
+    {
+        todos:
+            [
+                { id: 1, userId: 'jk', text: 'meeting@10am', startTime: '', completed: false }, 
+                { id: 2, userId: 'jk', text: 'collect test report', completed: false }, 
+                { id: 3, userId: 'jk', text: 'pay bill', completed: false }, 
+                { id: 4, userId: 'jk', text: 'Check with Ryan', completed: true },
+                { id: 1, userId: 'kk', text: 'Visit bank', completed: true }
+            ]
+    };
