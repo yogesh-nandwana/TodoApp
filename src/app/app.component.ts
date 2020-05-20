3699501686dc7c2edfb6229e7c611848809a315b
Yogesh Nandwana <yogesh.nandwana@gmail.com>
2020-05-20 09:30:46 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TodoApp';
+  appTitle = 'My Todo`s';
+  todosObj =
+    {
+        todos:
+            [
+                { id: 1, userId: 'jk', text: 'meeting@10am', startTime: '', completed: false }, 
+                { id: 2, userId: 'jk', text: 'collect test report', completed: false }, 
+                { id: 3, userId: 'jk', text: 'pay bill', completed: false }, 
+                { id: 4, userId: 'jk', text: 'Check with Ryan', completed: true },
+                { id: 1, userId: 'kk', text: 'Visit bank', completed: true }
+            ]
+    };
 }
--- src/todos.ts
+++ src/todos.ts
@@ -1,4 +1,4 @@
-export let todos =
+export let todosObj =
     {
         todos:
             [
